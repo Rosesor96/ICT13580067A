3d390bc66bad7e9913c3aefc28372cddd2757548
Pichaya1825 <boonyanan_p@su.ac.th>
2017-12-19 15:41:12 +0700

    project1
    send

---
+++ ICT13580067A/ICT13580067APage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Xamarin.Forms;
+
+namespace ICT13580067A
+{
+    public partial class ICT13580067APage : ContentPage
+    {
+        public ICT13580067APage()
+        {
+            InitializeComponent();
+            okButton.Clicked += OkButton_Clicked1;
+
+			genderPicker.Items.Add("ชาย");
+			genderPicker.Items.Add("หญิง");
+
+            mySlider.ValueChanged += MySlider_ValueChanged1;
+
+            myStepper.ValueChanged += MyStepper_ValueChanged1;
+        }
+
+        void OkButton_Clicked1(object sender, System.EventArgs e)
+        {
+            firstNameLabel.Text = firstNameEntry.Text;
+
+        }
+
+        void MySlider_ValueChanged1(object sender, ValueChangedEventArgs e)
+        {
+			int value = (int)e.NewValue;
+			valueLabel.Text = value.ToString();
+        }
+
+        void MyStepper_ValueChanged1(object sender, ValueChangedEventArgs e)
+        {
+			int value = (int)e.NewValue;
+			valueLabel.Text = value.ToString();
+        }
+
+		
+    }
+}
---
+++ ICT13580067A/MyPage3.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ICT13580067A
+{
+    public partial class MyPage3 : ContentPage
+    {
+        public MyPage3()
+        {
+            InitializeComponent();
+            backButton.Clicked += BackButton_Clicked;
+        }
+		void BackButton_Clicked(object sender, EventArgs e)
+		{
+			Navigation.PopModalAsync();
+		}
+    }
+}
